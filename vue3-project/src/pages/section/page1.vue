78d08e9304f20a8af190142b5e592ef3c7e733af
xue ming <810505339@qq.com>
2023-09-30 02:07:48 +0800

    测试登录

--- vue3-project/src/pages/section/page1.vue
+++ vue3-project/src/pages/section/page1.vue
@@ -1,7 +1,26 @@
 <template>
-  <view>1231232131</view>
+  <view>
+    <button open-type="getUserInfo" @click="getUserInfo" @bindtap="getUserInfo">登录</button>
+    <view v-if="userInfo.avatarUrl">
+      <view>{{userInfo.nickName}}</view>
+      <image :src="userInfo.avatarUrl"/>
+      <view>{{gender}}</view>
+    </view>
+  </view>
 </template>
 <script setup lang="ts">
+import {ref,onMounted,nextTick,computed} from 'vue';
+const userInfo=ref({
+  avatarUrl: "",
+city: "",
+country: "",
+gender: 0,
+language: "",
+nickName: "",
+province: "",
+})
+const gender=computed(()=>userInfo.value.gender===0?'男':'女')
+
 
 async function getOpenId() {
    wx.cloud.callFunction({
@@ -14,10 +33,27 @@ async function getOpenId() {
       type: 'getOpenId'
     }
   }).then(res=>{
-    console.log(res.result.userInfo.openId)
+    console.log(res.result)
   })
 }
 
-getOpenId()
+
+
+function getUserInfo(){
+  wx.getUserProfile({
+    desc: '用于完善会员资料', 
+    success: (res) => {
+      console.log(res);
+      userInfo.value=res.userInfo
+    }})
+
+}
+onMounted(()=>{
+
+
+  
+})
+
+
 
 </script>
