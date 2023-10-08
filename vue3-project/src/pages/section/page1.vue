10e8626d61f95799448ee22e60faf0dcf9963337
宋清餐 <623334054@qq.com>
2023-10-08 20:08:33 +0800

    feat: p2demo

--- vue3-project/src/pages/section/page1.vue
+++ vue3-project/src/pages/section/page1.vue
@@ -1,62 +1,68 @@
 <template>
-  <view class="full" @click="trigger"></view>
-  <view class="box_warper">
-    <view class="box" :animation="animationData.left">黄色叶子</view>
-    <view class="box" :animation="animationData.right">绿色叶子</view>
-  </view>
+	<view class="full" @click="trigger"></view>
+	<view class="box_warper">
+		<view class="box" :animation="animationData.left">黄色叶子</view>
+		<view class="box" :animation="animationData.right">绿色叶子</view>
+	</view>
 </template>
 <script setup lang="ts">
-import {ref} from 'vue';
+	import {
+		ref,
+		onMounted
+	} from "vue";
+	onMounted(() => {
+		getAnimation()
+	})
 
-const animationOption:UniApp.CreateAnimationOptions ={
-      duration: 3500,
-      timingFunction: 'ease',
-    }
-const animationData=ref({
-  left:'',
-  right:'',
-  distance:200
- 
-})
+	const animationOption: UniApp.CreateAnimationOptions = {
+		duration: 3500,
+		timingFunction: "ease",
+	};
+	const animationData = ref({
+		left: "",
+		right: "",
+		distance: 200,
+	});
 
-function getAnimation(){
-  const {distance} =animationData.value
-  const leftAnimation= uni.createAnimation(animationOption)
-  const rightAnimation= uni.createAnimation(animationOption)
-  leftAnimation.translateX(-distance).step()
-  rightAnimation.translateX(distance).step()
-  animationData.value.left=leftAnimation.export()
-  animationData.value.right=rightAnimation.export()
-}
+	function getAnimation() {
+		const {
+			distance
+		} = animationData.value;
+		const leftAnimation = uni.createAnimation(animationOption);
+		const rightAnimation = uni.createAnimation(animationOption);
+		leftAnimation.translateX(-distance).step();
+		rightAnimation.translateX(distance).step();
+		animationData.value.left = leftAnimation.export();
+		animationData.value.right = rightAnimation.export();
+	}
 
-function trigger(){
-  getAnimation()
-}
-
-
-
+	function trigger() {
+		getAnimation();
+	}
 </script>
 
 <style>
-.full{
-  position: absolute;
-  z-index: 10;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
-}
-  .box{
-    border:1px  solid #999;
-    height: 100vh;
-    width: 50%;
-    display: flex;
-    align-items: center;
-    text-align: center;
-    justify-content: center;
-  }
-  .box_warper{
-    display: flex;
-    text-align: center;
-  }
+	.full {
+		position: absolute;
+		z-index: 10;
+		top: 0;
+		right: 0;
+		left: 0;
+		bottom: 0;
+	}
+
+	.box {
+		border: 1px solid #999;
+		height: 100vh;
+		width: 50%;
+		display: flex;
+		align-items: center;
+		text-align: center;
+		justify-content: center;
+	}
+
+	.box_warper {
+		display: flex;
+		text-align: center;
+	}
 </style>
--- vue3-project/src/pages/section/page2.vue
+++ vue3-project/src/pages/section/page2.vue
@@ -1,4 +1,128 @@
 <template>
-  <view>page2</view>
+	<view class="bg"></view>
+	<view class="title" :animation="animationData.title">开业信息</view>
+	<view class="text_youliao" :animation="animationData.youliao">有料棕</view>
+	<view class="text_zhiyu" :animation="animationData.zhiyu">治愈绿</view>
+	<image src="../../static/bg_move.png" mode="cover" class="to_right" :animation="animationData.toRight" />
+	<image src="../../static/bg_move.png" mode="cover" class="to_left" :animation="animationData.toLeft" />
+
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import {
+		ref,
+		onMounted
+	} from "vue";
+
+	onMounted(() => {
+		getAnimation();
+	});
+
+	const animationOption: UniApp.CreateAnimationOptions = {
+		duration: 3000,
+		timingFunction: "ease",
+	};
+
+	const animationData = ref({
+		title: "",
+		toRight: '',
+		toLeft: '',
+		youliao: '',
+		zhiyu: '',
+	});
+	const distance = ref({
+		title: -250,
+		toRight: '100VW',
+		toLeft: '-100VW',
+	});
+	const getTextAnimation = () => { //字体淡入
+		const textAnimation1 = uni.createAnimation(animationOption);
+		const textAnimation2 = uni.createAnimation(animationOption);
+		textAnimation1.opacity(1).step();
+		textAnimation2.opacity(1).step();
+
+		animationData.value.youliao = textAnimation1.export()
+		animationData.value.zhiyu = textAnimation2.export()
+	}
+
+	function getAnimation() {
+		const {
+			title,
+			toRight,
+			toLeft
+		} = distance.value;
+		const titleAnimation = uni.createAnimation(animationOption);
+		const toRightAnimation = uni.createAnimation(animationOption);
+		const toLeftAnimation = uni.createAnimation(animationOption);
+		titleAnimation.translateY(title).step();
+		toRightAnimation.translateX(toRight).step();
+		toLeftAnimation.translateX(toLeft).step();
+		animationData.value.title = titleAnimation.export();
+		animationData.value.toRight = toRightAnimation.export();
+		animationData.value.toLeft = toLeftAnimation.export();
+		setTimeout(() => {
+			getTextAnimation()
+		}, 2e3)
+
+	}
+</script>
+<style>
+	.bg {
+		width: 100%;
+		height: 100vh;
+		background-color: #4472C4;
+		position: fixed;
+		top: 0;
+	}
+
+	.title {
+		position: absolute;
+		top: 50%;
+		left: calc((100% - 200rpx) /2);
+		width: 200rpx;
+		height: 20vh;
+		text-align: center;
+	}
+
+	.to_right {
+		width: 100%;
+		height: 500rpx;
+		position: absolute;
+		top: 25%;
+		left: -100%;
+		background: #99b4d3;
+		-webkit-clip-path: polygon(0 0, 76% 0, 24% 100%, 0% 100%);
+		clip-path: polygon(0 0, 250% 0, 0% 100%, 0% 100%);
+	}
+
+
+	.to_left {
+		width: 100%;
+		height: 500rpx;
+		position: absolute;
+		top: 50%;
+		left: 100%;
+		background: #99b4d3;
+		-webkit-clip-path: polygon(0 0, 76% 0, 24% 100%, 0% 100%);
+		clip-path: polygon(100% 0%, 100% 0%, -150% 100%, 100% 100%)
+	}
+
+	.text_youliao {
+		position: absolute;
+		top: 30%;
+		left: 54%;
+		font-size: 100rpx;
+		color: #825D45;
+		z-index: 999;
+		opacity: 0;
+	}
+
+	.text_zhiyu {
+		font-size: 100rpx;
+		color: #B1D026;
+		position: absolute;
+		top: 72%;
+		right: 54%;
+		z-index: 999;
+		opacity: 0;
+	}
+</style>
