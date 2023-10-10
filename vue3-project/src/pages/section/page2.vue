bb8d11eedb77fca426d1781b65a05684c5f9a3e4
songqingcan <921541509@qq.com>
2023-10-10 18:51:48 +0800

    feat: 券1页面完成

--- vue3-project/src/pages/section/page2.vue
+++ vue3-project/src/pages/section/page2.vue
@@ -1,398 +1,447 @@
 <template>
-	<view class="bg" @click="onNext"></view>
-	<view :style="{display:flag==0?'block':'none'}">
-		<view class="head" :animation="animationData.title">
-			<image :src="title" mode="cover" class="title" />
-			<image :src="head_bg" mode="cover" class="title_bg" />
-		</view>
-		<image :src="youliao" mode="cover" class="text_youliao" :animation="animationData.youliao" />
-		<image :src="zhiyu" mode="cover" class="text_zhiyu" :animation="animationData.zhiyu" />
-		<image :src="to_right" mode="cover" class="to_right" :animation="animationData.toRight" @click="onNext" />
-		<image :src="to_left" mode="cover" class="to_left" :animation="animationData.toLeft" @click="onNext" />
-		<image :src="or" mode="cover" class="or" :animation="animationData.or" />
-		<view class="box">
-			<image :src="xiongDa" mode="cover" class="xiongDa" :animation="animationData.xiongDa" />
-			<image :src="xiongEr" mode="cover" class="xiongEr" :animation="animationData.xiongEr" />
-		</view>
-	</view>
-
-
+  <view class="bg">
+    <view>
+      <view class="head" :animation="animationData.title">
+        <image :src="title" mode="cover" class="title" />
+        <image :src="head_bg" mode="cover" class="title_bg" />
+      </view>
+      <image
+        :src="youliao"
+        mode="cover"
+        class="text_youliao"
+        :animation="animationData.youliao"
+      />
+      <image
+        :src="zhiyu"
+        mode="cover"
+        class="text_zhiyu"
+        :animation="animationData.zhiyu"
+      />
+      <image
+        :src="to_right"
+        mode="cover"
+        class="to_right"
+        :animation="animationData.toRight"
+      />
+      <image
+        :src="to_left"
+        mode="cover"
+        class="to_left"
+        :animation="animationData.toLeft"
+      />
+      <view
+        class="to_right_click"
+        :style="{ display: flag > -1 ? 'block' : 'none' }"
+        @click="onNext(1)"
+      ></view>
+      <view
+        class="to_left_click"
+        :style="{ display: flag > -1 ? 'block' : 'none' }"
+        @click="onNext(2)"
+      ></view>
+      <image :src="or" mode="cover" class="or" :animation="animationData.or" />
+      <view class="box">
+        <image
+          :src="xiongDa"
+          mode="cover"
+          class="xiongDa"
+          :animation="animationData.xiongDa"
+        />
+        <image
+          :src="xiongEr"
+          mode="cover"
+          class="xiongEr"
+          :animation="animationData.xiongEr"
+        />
+      </view>
+    </view>
+  </view>
 </template>
 <script setup lang="ts">
-	import {
-		ref,
-		onMounted,
-		onBeforeUnmount
-	} from "vue";
-	import {
-		getAnimation
-	} from '@/utils/until.js'
-	import {
-		sleep
-	} from '@/utils/sleep'
-	import title from '@/static/img/page2/title.png'
-	import head_bg from '@/static/img/page2/head_bg.png'
-	import to_right from '@/static/img/page2/to_right.png'
-	import to_left from '@/static/img/page2/to_left.png'
-	import youliao from '@/static/img/page2/youliao.png'
-	import zhiyu from '@/static/img/page2/zhiyu.png'
-	import xiongDa from '@/static/img/page2/xiong1.png'
-	import xiongEr from '@/static/img/page2/xiong2.png'
-	import or from '@/static/img/page2/or.png'
-	let timer1 = null,
-		tmer2 = null
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { getAnimation } from "@/utils/until.js";
+import { sleep } from "@/utils/sleep";
+import title from "@/static/img/page2/title.png";
+import head_bg from "@/static/img/page2/head_bg.png";
+import to_right from "@/static/img/page2/to_right.png";
+import to_left from "@/static/img/page2/to_left.png";
+import youliao from "@/static/img/page2/youliao.png";
+import zhiyu from "@/static/img/page2/zhiyu.png";
+import xiongDa from "@/static/img/page2/xiong1.png";
+import xiongEr from "@/static/img/page2/xiong2.png";
+import or from "@/static/img/page2/or.png";
+let timer1: any = null;
 
+onMounted(async () => {
+  await getAnimation(animationStep, animationData);
+  flag.value = 0;
+  await getAnimation(youliaoAnimation, animationData);
+  timer1 = setInterval(async () => {
+    await getAnimation(youliaoAnimation, animationData);
+    await getAnimation(zhiyuAnimation, animationData);
+  }, 3500);
+  getAnimation(zhiyuAnimation, animationData);
+});
+onBeforeUnmount(() => {
+  clearInterval(timer1);
+});
 
-	onMounted(async () => {
-		await getAnimation(animationStep, animationData);
-		await getAnimation(youliaoAnimation, animationData);
-		timer1 = setInterval(async () => {
-			await getAnimation(youliaoAnimation, animationData);
-			await getAnimation(zhiyuAnimation, animationData);
-		}, 3500)
-		getAnimation(zhiyuAnimation, animationData);
-	});
-	onBeforeUnmount(() => {
-		clearInterval()
-	})
+const flag = ref(-1);
+const animationData = ref({
+  title: "",
+  toRight: "",
+  toLeft: "",
+  youliao: "",
+  zhiyu: "",
+  xiongDa: "",
+  xiongEr: "",
+  or: "",
+});
+const youliaoAnimation = ref({
+  youliao: {
+    action: {
+      opacity: 1,
+      rotateZ: 15,
+      scaleX: 1.2,
+      scaleY: 1.2,
+    },
+    duration: 500,
+    sleep: 500,
+    key: "youliao",
+  },
+  youliao1: {
+    action: {
+      rotateZ: -15,
+      scaleX: 0.8,
+      scaleY: 0.8,
+    },
+    duration: 200,
+    sleep: 500,
+    key: "youliao",
+  },
+  youliao2: {
+    action: {
+      rotateZ: 0,
+      scaleX: 1.0,
+      scaleY: 1.0,
+    },
+    duration: 200,
+    sleep: 500,
+    key: "youliao",
+  },
+});
+const zhiyuAnimation = ref({
+  zhiyu: {
+    action: {
+      opacity: 1,
+      rotateZ: -15,
+      scaleX: 1.2,
+      scaleY: 1.2,
+    },
+    duration: 500,
+    sleep: 500,
+    key: "zhiyu",
+  },
+  zhiyu1: {
+    action: {
+      rotateZ: 15,
+      scaleX: 0.8,
+      scaleY: 0.8,
+    },
+    duration: 200,
+    sleep: 500,
+    key: "zhiyu",
+  },
+  zhiyu2: {
+    action: {
+      rotateZ: 0,
+      scaleX: 1.0,
+      scaleY: 1.0,
+    },
+    duration: 200,
+    sleep: 500,
+    key: "zhiyu",
+  },
+});
+const animationStep = ref({
+  titleShow: {
+    action: {
+      opacity: 1,
+    },
+    duration: 3000,
+    sleep: 3000,
+    key: "title",
+  },
+  titleMove: {
+    action: {
+      translateY: "-453rpx",
+    },
+    duration: 1000,
+    sleep: 500,
+    key: "title",
+  },
+  toRight: {
+    action: {
+      translateX: "790rpx",
+    },
+    duration: 1500,
+    key: "toRight",
+  },
+  toLeft: {
+    action: {
+      translateX: "-710rpx",
+    },
+    duration: 1500,
+    sleep: 1000,
+    key: "toLeft",
+  },
+  toRight1: {
+    action: {
+      translateX: "820rpx",
+    },
+    duration: 500,
+    key: "toRight",
+  },
+  toLeft1: {
+    action: {
+      translateX: "-740rpx",
+    },
+    duration: 500,
+    sleep: 500,
+    key: "toLeft",
+  },
+  toRight2: {
+    action: {
+      translateX: "790rpx",
+    },
+    duration: 500,
+    key: "toRight",
+  },
+  toLeft2: {
+    action: {
+      translateX: "-710rpx",
+    },
+    duration: 500,
+    sleep: 500,
+    key: "toLeft",
+  },
+  youliao: {
+    action: {
+      opacity: 1,
+    },
+    duration: 3000,
+    key: "youliao",
+  },
+  zhiyu: {
+    action: {
+      opacity: 1,
+    },
+    duration: 3000,
+    key: "zhiyu",
+  },
+  // xiongDa: {
+  // 	action: {
+  // 		translateX: '250rpx'
+  // 	},
+  // 	duration: 2000,
+  // 	key: 'xiongDa'
+  // },
+  // xiongEr: {
+  // 	action: {
+  // 		translateX: '-250rpx'
+  // 	},
+  // 	duration: 2000,
+  // 	sleep: 1500,
+  // 	key: 'xiongEr'
+  // },
+  xiongDa1: {
+    action: {
+      translateX: "215rpx",
+    },
+    duration: 500,
+    key: "xiongDa",
+  },
+  xiongEr2: {
+    action: {
+      translateX: "-215rpx",
+    },
+    duration: 500,
+    key: "xiongEr",
+  },
+  or: {
+    action: {
+      opacity: 1,
+      rotateZ: 15,
+      scaleX: 1.2,
+      scaleY: 1.2,
+    },
+    duration: 400,
+    sleep: 500,
+    key: "or",
+  },
+  or1: {
+    action: {
+      rotateZ: -15,
+      scaleX: 0.8,
+      scaleY: 0.8,
+    },
+    duration: 200,
+    sleep: 500,
+    key: "or",
+  },
+  or2: {
+    action: {
+      rotateZ: 0,
+      scaleX: 1.0,
+      scaleY: 1.0,
+    },
+    duration: 200,
+    sleep: 500,
+    key: "or",
+  },
+});
 
-	const flag = ref(0)
-	const animationData = ref({
-		title: "",
-		toRight: '',
-		toLeft: '',
-		youliao: '',
-		zhiyu: '',
-		xiongDa: '',
-		xiongEr: '',
-		or: ''
-	});
-	const youliaoAnimation = ref({
-		youliao: {
-			action: {
-				opacity: 1,
-				rotateZ: 15,
-				scaleX: 1.2,
-				scaleY: 1.2,
-			},
-			duration: 500,
-			sleep: 500,
-			key: "youliao"
-		},
-		youliao1: {
-			action: {
-				rotateZ: -15,
-				scaleX: 0.8,
-				scaleY: 0.8,
-			},
-			duration: 200,
-			sleep: 500,
-			key: 'youliao'
-		},
-		youliao2: {
-			action: {
-				rotateZ: 0,
-				scaleX: 1.0,
-				scaleY: 1.0,
-			},
-			duration: 200,
-			sleep: 500,
-			key: 'youliao'
-		},
+const onNext = (type: any) => {
+  flag.value = type;
 
-	})
-	const zhiyuAnimation = ref({
-		zhiyu: {
-			action: {
-				opacity: 1,
-				rotateZ: -15,
-				scaleX: 1.2,
-				scaleY: 1.2,
-			},
-			duration: 500,
-			sleep: 500,
-			key: "zhiyu"
-		},
-		zhiyu1: {
-			action: {
-				rotateZ: 15,
-				scaleX: 0.8,
-				scaleY: 0.8,
-			},
-			duration: 200,
-			sleep: 500,
-			key: 'zhiyu'
-		},
-		zhiyu2: {
-			action: {
-				rotateZ: 0,
-				scaleX: 1.0,
-				scaleY: 1.0,
-			},
-			duration: 200,
-			sleep: 500,
-			key: 'zhiyu'
-		},
-	})
-	const animationStep = ref({
-		titleShow: {
-			action: {
-				opacity: 1,
-			},
-			duration: 3000,
-			sleep: 3000,
-			key: "title"
-		},
-		titleMove: {
-			action: {
-				translateY: '-453rpx'
-			},
-			duration: 1000,
-			sleep: 500,
-			key: "title"
-		},
-		toRight: {
-			action: {
-				translateX: '790rpx'
-			},
-			duration: 1500,
-			key: 'toRight'
-		},
-		toLeft: {
-			action: {
-				translateX: '-710rpx'
-			},
-			duration: 1500,
-			sleep: 1000,
-			key: 'toLeft'
-		},
-		toRight1: {
-			action: {
-				translateX: '820rpx'
-			},
-			duration: 500,
-			key: 'toRight'
-		},
-		toLeft1: {
-			action: {
-				translateX: '-740rpx'
-			},
-			duration: 500,
-			sleep: 500,
-			key: 'toLeft'
-		},
-		toRight2: {
-			action: {
-				translateX: '790rpx'
-			},
-			duration: 500,
-			key: 'toRight'
-		},
-		toLeft2: {
-			action: {
-				translateX: '-710rpx'
-			},
-			duration: 500,
-			sleep: 500,
-			key: 'toLeft'
-		},
-		youliao: {
-			action: {
-				opacity: 1
-			},
-			duration: 3000,
-			key: 'youliao'
-		},
-		zhiyu: {
-			action: {
-				opacity: 1
-			},
-			duration: 3000,
-			key: 'zhiyu'
-		},
-		// xiongDa: {
-		// 	action: {
-		// 		translateX: '250rpx'
-		// 	},
-		// 	duration: 2000,
-		// 	key: 'xiongDa'
-		// },
-		// xiongEr: {
-		// 	action: {
-		// 		translateX: '-250rpx'
-		// 	},
-		// 	duration: 2000,
-		// 	sleep: 1500,
-		// 	key: 'xiongEr'
-		// },
-		xiongDa1: {
-			action: {
-				translateX: '215rpx'
-			},
-			duration: 500,
-			key: 'xiongDa'
-		},
-		xiongEr2: {
-			action: {
-				translateX: '-215rpx'
-			},
-			duration: 500,
-			key: 'xiongEr'
-		},
-		or: {
-			action: {
-				opacity: 1,
-				rotateZ: 15,
-				scaleX: 1.2,
-				scaleY: 1.2,
-			},
-			duration: 400,
-			sleep: 500,
-			key: 'or',
-		},
-		or1: {
-			action: {
-				rotateZ: -15,
-				scaleX: 0.8,
-				scaleY: 0.8,
-			},
-			duration: 200,
-			sleep: 500,
-			key: 'or'
-		},
-		or2: {
-			action: {
-				rotateZ: 0,
-				scaleX: 1.0,
-				scaleY: 1.0,
-			},
-			duration: 200,
-			sleep: 500,
-			key: 'or'
-		},
-	});
-
-
-	const onNext = (type) => {
-		console.log(1111, type)
-		flag.value = type
-	}
+  //跳转第领券页面
+  uni.navigateTo({
+    url: "/pages/section/page3?flag=" + type,
+    animationType: "none",
+    animationDuration: 200,
+  });
+};
 </script>
 <style>
-	.bg {
-		width: 100%;
-		height: 100vh;
-		background-color: #FEFFE8;
-		position: fixed;
-		top: 0;
-	}
+.bg {
+  width: 100%;
+  height: 100vh;
+  background-color: #feffe8;
+  position: fixed;
+  top: 0;
+}
 
-	.head {
-		opacity: 0;
-		position: absolute;
-		top: calc((100% - 508rpx) /2);
-		left: calc((100% - 676rpx) /2);
-		width: 676rpx;
-		height: 508rpx;
-		text-align: center;
-	}
+.head {
+  opacity: 0;
+  position: absolute;
+  top: calc((100% - 508rpx) / 2);
+  left: calc((100% - 676rpx) / 2);
+  width: 676rpx;
+  height: 508rpx;
+  text-align: center;
+}
 
-	.title {
-		position: absolute;
-		top: calc((100% - 254rpx - 220rpx) /2);
-		left: calc((100% - 312rpx) /2);
-		width: 312rpx;
-		height: 254rpx;
-		text-align: center;
-		z-index: 2;
-	}
+.title {
+  position: absolute;
+  top: calc((100% - 254rpx - 220rpx) / 2);
+  left: calc((100% - 312rpx) / 2);
+  width: 312rpx;
+  height: 254rpx;
+  text-align: center;
+  z-index: 2;
+}
 
-	.title_bg {
-		position: absolute;
-		top: calc((100% - 508rpx) /2);
-		left: calc((100% - 676rpx) /2);
-		width: 676rpx;
-		height: 508rpx;
-		text-align: center;
-	}
+.title_bg {
+  position: absolute;
+  top: calc((100% - 508rpx) / 2);
+  left: calc((100% - 676rpx) / 2);
+  width: 676rpx;
+  height: 508rpx;
+  text-align: center;
+}
 
-	.to_right {
-		width: 665rpx;
-		height: 518rpx;
-		position: absolute;
-		top: 341rpx;
-		left: -100%;
-		background: #99b4d3;
-		-webkit-clip-path: polygon(0 0, 76% 0, 24% 100%, 0% 100%);
-		clip-path: polygon(0 0, 250% 0, 0% 100%, 0% 100%);
-	}
+.to_right {
+  width: 665rpx;
+  height: 518rpx;
+  position: absolute;
+  top: 341rpx;
+  left: -100%;
+  background: #99b4d3;
+  -webkit-clip-path: polygon(0 0, 76% 0, 24% 100%, 0% 100%);
+  clip-path: polygon(0 0, 250% 0, 0% 100%, 0% 100%);
+}
 
-	.to_left {
-		width: 665rpx;
-		height: 518rpx;
-		position: absolute;
-		top: 685rpx;
-		left: 100%;
-		background: #99b4d3;
-		-webkit-clip-path: polygon(0 0, 76% 0, 24% 100%, 0% 100%);
-		clip-path: polygon(100% 0%, 100% 0%, -150% 100%, 100% 100%)
-	}
+.to_left {
+  width: 665rpx;
+  height: 518rpx;
+  position: absolute;
+  top: 685rpx;
+  left: 100%;
+  background: #99b4d3;
+  -webkit-clip-path: polygon(0 0, 76% 0, 24% 100%, 0% 100%);
+  clip-path: polygon(100% 0%, 100% 0%, -150% 100%, 100% 100%);
+}
+.to_right_click {
+  width: 665rpx;
+  height: 448rpx;
+  position: absolute;
+  top: 341rpx;
+  z-index: 20;
+  left: calc(790rpx - 100%);
+}
+.to_left_click {
+  width: 665rpx;
+  height: 448rpx;
+  position: absolute;
+  top: 790rpx;
+  z-index: 20;
+  left: calc(100% - 710rpx);
+}
+.text_youliao {
+  position: absolute;
+  top: 461rpx;
+  left: 223rpx;
+  font-size: 100rpx;
+  color: #825d45;
+  z-index: 2;
+  opacity: 0;
+  width: 447rpx;
+  height: 139rpx;
+}
 
-	.text_youliao {
-		position: absolute;
-		top: 461rpx;
-		left: 223rpx;
-		font-size: 100rpx;
-		color: #825D45;
-		z-index: 2;
-		opacity: 0;
-		width: 447rpx;
-		height: 139rpx;
-	}
+.text_zhiyu {
+  font-size: 100rpx;
+  color: #b1d026;
+  position: absolute;
+  top: 956rpx;
+  right: 225rpx;
+  z-index: 2;
+  opacity: 0;
+  width: 447rpx;
+  height: 145rpx;
+}
 
-	.text_zhiyu {
-		font-size: 100rpx;
-		color: #B1D026;
-		position: absolute;
-		top: 956rpx;
-		right: 225rpx;
-		z-index: 2;
-		opacity: 0;
-		width: 447rpx;
-		height: 145rpx;
-	}
+.or {
+  width: 292rpx;
+  height: 171rpx;
+  position: absolute;
+  top: 699rpx;
+  left: 226rpx;
+  opacity: 0;
+}
 
-	.or {
-		width: 292rpx;
-		height: 171rpx;
-		position: absolute;
-		top: 699rpx;
-		left: 226rpx;
-		opacity: 0;
-	}
+.box {
+  position: fixed;
+  left: 37rpx;
+  width: 673rpx;
+  height: 100vh;
+  overflow: hidden;
+  z-index: 3;
+}
 
-	.box {
-		position: fixed;
-		left: 37rpx;
-		width: 673rpx;
-		height: 100vh;
-		overflow: hidden;
-		z-index: 3;
-	}
+.xiongDa {
+  width: 215rpx;
+  height: 354rpx;
+  position: absolute;
+  top: 408rpx;
+  left: calc(36rpx - 250rpx);
+}
 
-	.xiongDa {
-		width: 215rpx;
-		height: 354rpx;
-		position: absolute;
-		top: 408rpx;
-		left: calc(36rpx - 250rpx);
-	}
-
-	.xiongEr {
-		width: 215rpx;
-		height: 354rpx;
-		position: absolute;
-		top: 799rpx;
-		right: calc(36rpx - 250rpx);
-	}
+.xiongEr {
+  width: 215rpx;
+  height: 354rpx;
+  position: absolute;
+  top: 799rpx;
+  right: calc(36rpx - 250rpx);
+}
 </style>
--- vue3-project/src/pages/section/page3.vue
+++ vue3-project/src/pages/section/page3.vue
@@ -1,4 +1,307 @@
 <template>
-  <view>page3</view>
+  <view class="main">
+    <image :src="bg" mode="cover" class="bg" />
+    <image
+      :src="model_box"
+      mode="cover"
+      class="model_box"
+      :animation="animationData.model_box"
+    />
+    <image
+      :src="text_1"
+      mode="cover"
+      class="text_1"
+      :animation="animationData.text_1"
+    />
+    <image
+      :src="text_2"
+      mode="cover"
+      class="text_2"
+      :animation="animationData.text_2"
+    />
+    <image
+      :src="text_3"
+      mode="cover"
+      class="text_3"
+      :animation="animationData.text_3"
+    />
+    <image
+      :src="ticket"
+      mode="cover"
+      class="ticket"
+      :animation="animationData.ticket"
+    />
+    <image
+      :src="tips"
+      mode="cover"
+      class="tips"
+      :animation="animationData.tips"
+    />
+    <image
+      :src="detail"
+      mode="cover"
+      class="detail"
+      :animation="animationData.detail"
+    />
+    <image :src="btn" mode="cover" class="btn" :animation="animationData.btn" />
+  </view>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { getAnimation } from "@/utils/until.js";
+import { sleep } from "@/utils/sleep";
+import bg from "@/static/img/page3/bg.png";
+import model_box from "@/static/img/page3/model_box.png";
+import text_1 from "@/static/img/page3/text_1.png";
+import text_2 from "@/static/img/page3/text_2.png";
+import text_3 from "@/static/img/page3/text_3.png";
+import ticket from "@/static/img/page3/ticket.png";
+import tips from "@/static/img/page3/tips.png";
+import detail from "@/static/img/page3/detail.png";
+import btn from "@/static/img/page3/btn.png";
+
+import { onLoad } from "@dcloudio/uni-app";
+let timer1: any = null;
+onLoad((option) => {
+  console.log(option);
+  type.value = option?.flag;
+});
+onMounted(async () => {
+  await getAnimation(animationStep, animationData);
+  timer1 = setInterval(async () => {
+    await getAnimation(btnAnimationData, animationData);
+  }, 2000);
+});
+onBeforeUnmount(() => {
+  clearInterval(timer1);
+});
+
+const type = ref();
+const btnAnimationData = ref({
+  btn1: {
+    action: {
+      opacity: 1,
+      rotateZ: 15,
+      scaleX: 1.2,
+      scaleY: 1.2,
+    },
+    duration: 500,
+    sleep: 500,
+    key: "btn",
+  },
+  btn2: {
+    action: {
+      rotateZ: -10,
+      scaleX: 0.8,
+      scaleY: 0.8,
+    },
+    duration: 200,
+    sleep: 500,
+    key: "btn",
+  },
+  btn3: {
+    action: {
+      rotateZ: 0,
+      scaleX: 1.0,
+      scaleY: 1.0,
+    },
+    duration: 200,
+    sleep: 500,
+    key: "btn",
+  },
+});
+const animationData = ref({
+  model_box: "",
+  text_1: "",
+  text_2: "",
+  text_3: "",
+  ticket: "",
+  tips: "",
+  detail: "",
+  btn: "",
+});
+
+const animationStep = ref({
+  ticket_move: {
+    action: {
+      translateY: "100rpx",
+    },
+    duration: 1000,
+    sleep: 1000,
+    key: "ticket",
+  },
+  ticket_move2: {
+    action: {
+      translateX: "750rpx",
+    },
+    duration: 1000,
+    sleep: 200,
+    key: "ticket",
+  },
+  model_box: {
+    action: {
+      opacity: 1,
+    },
+    duration: 3000,
+    sleep: 1000,
+    key: "model_box",
+  },
+  model_box1: {
+    action: {
+      translateY: "-50rpx",
+    },
+    duration: 2000,
+    sleep: 500,
+    key: "model_box",
+  },
+  text_1_show: {
+    action: {
+      opacity: 1,
+    },
+    duration: 1000,
+    // sleep: 200,
+    key: "text_1",
+  },
+  text_1_move: {
+    action: {
+      translateY: "-30rpx",
+    },
+    duration: 1000,
+    sleep: 1000,
+    key: "text_1",
+  },
+  text_2_show: {
+    action: {
+      opacity: 1,
+    },
+    duration: 1000,
+    // sleep: 200,
+    key: "text_2",
+  },
+  text_2_move: {
+    action: {
+      translateY: "-30rpx",
+    },
+    duration: 1000,
+    sleep: 1000,
+    key: "text_2",
+  },
+  text_3_show: {
+    action: {
+      opacity: 1,
+    },
+    duration: 1000,
+    // sleep:200,
+    key: "text_3",
+  },
+  text_3_move: {
+    action: {
+      translateY: "-30rpx",
+    },
+    duration: 1000,
+    sleep: 1000,
+    key: "text_3",
+  },
+  tips: {
+    action: {
+      opacity: 1,
+    },
+    duration: 1000,
+    key: "tips",
+  },
+  detail: {
+    action: {
+      opacity: 1,
+    },
+    duration: 1000,
+    key: "detail",
+  },
+  btn: {
+    action: {
+      opacity: 1,
+    },
+    duration: 1000,
+    sleep: 1000,
+    key: "btn",
+  },
+});
+</script>
+<style>
+.main {
+  width: 100%;
+  height: 1333rpx;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.bg {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.model_box {
+  width: 727rpx;
+  height: 553rpx;
+  position: absolute;
+  top: 100rpx;
+  left: 18rpx;
+  opacity: 0;
+}
+.text_1 {
+  width: 404rpx;
+  height: 38rpx;
+  position: absolute;
+  top: 234rpx;
+  left: 86rpx;
+  opacity: 0;
+}
+.text_2 {
+  width: 329rpx;
+  height: 38rpx;
+  position: absolute;
+  top: 297rpx;
+  left: 138rpx;
+  opacity: 0;
+}
+.text_3 {
+  width: 568rpx;
+  height: 38rpx;
+  position: absolute;
+  top: 361rpx;
+  left: 84rpx;
+  opacity: 0;
+}
+.ticket {
+  width: 100%;
+  height: 713rpx;
+  position: absolute;
+  top: 411rpx;
+  left: -100%;
+}
+.tips {
+  width: 144rpx;
+  height: 45rpx;
+  position: absolute;
+  top: 965rpx;
+  left: 44rpx;
+  opacity: 0;
+}
+.detail {
+  width: 431rpx;
+  height: 161rpx;
+  position: absolute;
+  top: 1097rpx;
+  left: 20rpx;
+  opacity: 0;
+}
+.btn {
+  width: 250rpx;
+  height: 146rpx;
+  position: absolute;
+  top: 1140rpx;
+  left: 486rpx;
+  opacity: 0;
+}
+</style>
